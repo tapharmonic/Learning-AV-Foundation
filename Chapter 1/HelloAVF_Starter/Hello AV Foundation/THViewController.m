548ca5f300fe17579cfe30e74bddebc3c24566ef
Bob McCune <bob@tapharmonic.com>
2014-10-14 19:31:03 -0500

    Updates for iOS 8

--- Chapter 1/HelloAVF_Starter/Hello AV Foundation/THViewController.m
+++ Chapter 1/HelloAVF_Starter/Hello AV Foundation/THViewController.m
@@ -26,6 +26,7 @@
 #import "THViewController.h"
 #import "THSpeechController.h"
 #import <AVFoundation/AVFoundation.h>
+#import "THBubbleCell.h"
 
 @interface THViewController () <AVSpeechSynthesizerDelegate>
 @property (strong, nonatomic) THSpeechController *speechController;
@@ -36,6 +37,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.contentInset = UIEdgeInsetsMake(20.0f, 0.0f, 20.0f, 0.0f);
     self.speechController = [THSpeechController speechController];
     self.speechController.synthesizer.delegate = self;
     self.speechStrings = [NSMutableArray array];
@@ -47,15 +49,18 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *identifier = indexPath.row % 2 == 0 ? @"YouCell" : @"AVFCell";
 
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"VoiceCell" forIndexPath:indexPath];
-    cell.textLabel.text = self.speechStrings[indexPath.row];
+    THBubbleCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
+    cell.messageLabel.text = self.speechStrings[indexPath.row];
     return cell;
 }
 
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didStartSpeechUtterance:(AVSpeechUtterance *)utterance {
     [self.speechStrings addObject:utterance.speechString];
     [self.tableView reloadData];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:self.speechStrings.count - 1 inSection:0];
+    [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionBottom animated:YES];
 }
 
 @end
